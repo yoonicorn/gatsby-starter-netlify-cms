866462ec5f5555234dc8e2bf3161477b18aeb2e6
yoonicorn <yoonmatt@msu.edu>
2021-07-11 10:19:25 -0500

    centered sub

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -75,7 +75,7 @@ export const IndexPageTemplate = ({
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
                   <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
+                    <h3 className="subtitle is-center">{mainpitch.description}</h3>
                   </div>
                 </div>
                 <div className="columns">
